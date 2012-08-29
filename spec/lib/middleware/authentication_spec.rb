e4e789b8c411d24dbd27bac6884b162d93e972b6
Eric J. Holmes <eric@ejholmes.net>
2012-08-28 21:01:40 -0700

    Cleanup.

--- spec/lib/middleware/authentication_spec.rb
+++ spec/lib/middleware/authentication_spec.rb
@@ -3,17 +3,16 @@ require 'spec_helper'
 describe Restforce::Middleware::Authentication do
   include_context 'basic client'
 
-  let(:oauth_token) { '00Dx0000000BV7z!AR8AQAxo9UfVkh8AlV0Gomt9Czx9LjHnSSpwBMmbRcgKFmxOtvxjTrKW19ye6PE3Ds1eQz3z8jr3W7_VbWmEu4Q8TVGSTHxs' }
-
   describe 'authentication' do
     before do
       @requests = [].tap do |requests|
         requests << stub_request(:get, %r{/services/data/v24\.0/sobjects}).
-          with(:headers => {'Authorization' => 'OAuth'}).
+          with(:headers => {'Authorization' => 'OAuth bad_token'}).
           to_return(:status => 401, :body => fixture(:expired_session_response))
 
-        requests << stub_request(:get, 'https://login.salesforce.com/services/oauth2/authorize?client_id=client_id' \
-         '&client_secret=client_secret&grant_type=password&password=bar&username=foo').
+        requests << stub_request(:get, "https://login.salesforce.com/services/oauth2/authorize?" \
+         "client_id=#{client_options[:client_id]}&client_secret=#{client_options[:client_secret]}" \
+         "&grant_type=password&password=#{client_options[:password]}&username=#{client_options[:username]}").
          to_return(:status => 200, :body => fixture(:auth_success_response))
 
         requests << stub_request(:get, %r{/services/data/v24\.0/sobjects}).
@@ -31,7 +30,7 @@ describe Restforce::Middleware::Authentication do
     end
 
     context 'when a username and password is set' do
-      let(:client_options) { {:username => 'foo', :password => 'bar', :client_id => 'client_id', :client_secret => 'client_secret'} }
+      let(:client_options) { base_options.merge(:oauth_token => 'bad_token') }
 
       describe 'the client options' do
         subject { client.instance_variable_get :@options }
--- spec/support/basic_client.rb
+++ spec/support/basic_client.rb
@@ -1,4 +1,21 @@
 shared_context 'basic client' do
-  let(:client_options) { { :oauth_token => 'token' } }
+  let(:oauth_token)   { '00Dx0000000BV7z!AR8AQAxo9UfVkh8AlV0Gomt9Czx9LjHnSSpwBMmbRcgKFmxOtvxjTrKW19ye6PE3Ds1eQz3z8jr3W7_VbWmEu4Q8TVGSTHxs' }
+  let(:username)      { 'foo'           }
+  let(:password)      { 'bar'           }
+  let(:client_id)     { 'client_id'     }
+  let(:client_secret) { 'client_secret' }
+
+  let(:base_options) do
+    {
+      :oauth_token   => oauth_token,
+      :username      => username,
+      :password      => password,
+      :client_id     => client_id,
+      :client_secret => client_secret
+    }
+  end
+
+  let(:client_options) { base_options }
+
   let(:client) { Restforce::Client.new client_options }
 end
