a34d48576f9ad10f256ce34148fa783db952224c
Eric J. Holmes <eric@ejholmes.net>
2012-08-29 00:34:10 -0700

    ensure options are set

--- spec/lib/client_spec.rb
+++ spec/lib/client_spec.rb
@@ -3,6 +3,18 @@ require 'spec_helper'
 describe Restforce::Client do
   include_context 'basic client'
   
+  describe '@options' do
+    subject { client.instance_variable_get :@options }
+
+    its([:oauth_token])    { should eq oauth_token    }
+    its([:refresh_token])  { should eq refresh_token  }
+    its([:client_id])      { should eq client_id      }
+    its([:client_secret])  { should eq client_secret  }
+    its([:username])       { should eq username       }
+    its([:password])       { should eq password       }
+    its([:security_token]) { should eq security_token }
+  end
+  
   describe '.describe_sobjects' do
     subject { client.describe_sobjects }
 
--- spec/support/basic_client.rb
+++ spec/support/basic_client.rb
@@ -1,17 +1,23 @@
 shared_context 'basic client' do
-  let(:oauth_token)   { '00Dx0000000BV7z!AR8AQAxo9UfVkh8AlV0Gomt9Czx9LjHnSSpwBMmbRcgKFmxOtvxjTrKW19ye6PE3Ds1eQz3z8jr3W7_VbWmEu4Q8TVGSTHxs' }
-  let(:username)      { 'foo'           }
-  let(:password)      { 'bar'           }
-  let(:client_id)     { 'client_id'     }
-  let(:client_secret) { 'client_secret' }
+  let(:oauth_token)    { '00Dx0000000BV7z!AR8AQAxo9UfVkh8AlV0Gomt9Czx9LjHnSSpwBMmbRcgKFmxOtvxjTrKW19ye6PE3Ds1eQz3z8jr3W7_VbWmEu4Q8TVGSTHxs' }
+  let(:refresh_token)  { 'refresh' }
+  let(:instance_url)   { 'https://na1.salesforce.com' }
+  let(:username)       { 'foo'            }
+  let(:password)       { 'bar'            }
+  let(:security_token) { 'security_token' }
+  let(:client_id)      { 'client_id'      }
+  let(:client_secret)  { 'client_secret'  }
 
   let(:base_options) do
     {
-      :oauth_token   => oauth_token,
-      :username      => username,
-      :password      => password,
-      :client_id     => client_id,
-      :client_secret => client_secret
+      :oauth_token    => oauth_token,
+      :refresh_token  => refresh_token,
+      :instance_url   => instance_url,
+      :username       => username,
+      :password       => password,
+      :security_token => security_token,
+      :client_id      => client_id,
+      :client_secret  => client_secret
     }
   end
 
