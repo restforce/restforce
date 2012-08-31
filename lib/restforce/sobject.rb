51528bf78ae10adb253e3670abdd14f8b5b04c03
Eric J. Holmes <eric@ejholmes.net>
2012-08-31 13:49:15 -0700

    Convert nested collections.

--- lib/restforce/sobject.rb
+++ lib/restforce/sobject.rb
@@ -2,8 +2,10 @@ module Restforce
   class SObject < Hashie::Mash
     attr_reader :sobject_type
 
-    def initialize(source_hash = nil, client = nil, default = nil, &blk)
-      self.build(source_hash) if source_hash
+    def initialize(source_hash = nil, default = nil, &blk)
+      attributes = source_hash.delete('attributes')
+      @sobject_type = attributes['type']
+      deep_update(source_hash) if source_hash
       default ? super(default) : super(&blk)
     end
 
@@ -14,6 +16,26 @@ module Restforce
       @sobject_type = attributes['type']
       mash = Hashie::Mash.new(hash)
       self.replace(mash)
+    end
+
+    def convert_value(val, duping=false)
+      case val
+      when self.class
+        val.dup
+      when ::Hash
+        val = val.dup if duping
+        # If the hash has a 'records' key, then it's a collection
+        # of sobject records.
+        if val.has_key? 'records'
+          Restforce::Collection.new(val)
+        else
+          self.class.new(val)
+        end
+      when Array
+        val.collect{ |e| convert_value(e) }
+      else
+        val
+      end
     end
 
   end
--- spec/lib/sobject_spec.rb
+++ spec/lib/sobject_spec.rb
@@ -11,6 +11,28 @@ describe Restforce::SObject do
       it                 { should be_a Restforce::SObject }
       its(:sobject_type) { should eq 'Whizbang' }
       its(:Text_Label)   { should eq 'Hi there!' }
+
+      describe 'child records object' do
+        subject { record.Whizbangs__r }
+
+        it { should be_a Restforce::Collection }
+
+        describe 'each child record' do
+          it 'should be a Restforce::SObject' do
+            record.Whizbangs__r.each do |record|
+              record.should be_a Restforce::SObject
+            end
+          end
+        end
+      end
+
+      describe 'parent record object' do
+        subject { record.ParentWhizbang__r }
+
+        it { should be_a Restforce::SObject }
+        its(:sobject_type) { should eq 'Whizbang' }
+        its(:Name) { should eq 'Parent Whizbang' }
+      end
     end
   end
 end
