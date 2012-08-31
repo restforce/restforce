9bda1bf7c3b996c1fc62f764c38b90a1b63425ca
Eric J. Holmes <eric@ejholmes.net>
2012-08-30 23:27:21 -0700

    cleanup

--- spec/lib/client_spec.rb
+++ spec/lib/client_spec.rb
@@ -15,6 +15,44 @@ describe Restforce::Client do
     its([:security_token]) { should eq security_token }
   end
 
+  describe 'authentication' do
+    include_context 'basic client'
+    include_context 'authentication middleware'
+
+    describe 'password authentication' do
+      let(:authentication_request) { password_authentication_request }
+
+      context 'when a username and password is set' do
+        let(:expired_token)  { 'expired_token' }
+        let(:client_options) { base_options.merge(:oauth_token => expired_token) }
+
+        describe 'the client options' do
+          subject { client.instance_variable_get :@options }
+
+          its([:instance_url]) { should eq instance_url }
+          its([:oauth_token])  { should eq oauth_token  }
+        end
+      end
+    end
+
+    describe 'oauth authentication' do
+      let(:authentication_request) { oauth_refresh_authentication_request }
+
+      context 'oauth token and refresh token are set' do
+        let(:expired_token)  { 'expired_token' }
+        let(:client_options) { oauth_refresh_options.merge(:oauth_token => expired_token) }
+
+        describe 'the client options' do
+          subject { client.instance_variable_get :@options }
+
+          its([:instance_url])  { should eq instance_url  }
+          its([:oauth_token])   { should eq oauth_token   }
+          its([:refresh_token]) { should eq refresh_token }
+        end
+      end
+    end
+  end
+
   describe '.authentication_middleware' do
     subject { client.send :authentication_middleware }
 
--- spec/lib/middleware/instance_url_spec.rb
+++ spec/lib/middleware/instance_url_spec.rb
@@ -1,15 +1,37 @@
 require 'spec_helper'
 
 describe Restforce::Middleware::InstanceURL do
-  include_context 'basic client'
+  let(:app) { stub(call: nil) }
+  let(:client) { stub(send: mock(url_prefix: URI.parse('https://whiz.bang'))) }
+  let(:middleware) { described_class.new app, client, options }
+  
+  context 'when the instance url is not set' do
+    let(:options) { { :instance_url => nil } }
 
-  context 'when the instance url is missing' do
-    let(:client_options) { base_options.merge(:instance_url => nil) }
-
-    it 'raises an exception' do
+    it 'raises an error' do
       expect {
-        client.get '/foo'
-      }.to_not raise_error URI::InvalidURIError
+        middleware.call(nil)
+      }.to raise_error Faraday::Error::ClientError
+    end
+  end
+
+  context 'when the instance url is set' do
+    let(:options) { { :instance_url => 'https://foo.bar' } }
+
+    context 'and it does not match the connection url prefix' do
+      it 'raises an error' do
+        expect {
+          middleware.call(nil)
+        }.to raise_error Restforce::InstanceURLError
+      end
+    end
+
+    context 'and it matches the connection url prefix' do
+      let(:client) { stub(send: mock(url_prefix: URI.parse('https://foo.bar'))) }
+
+      it 'calls tne next middleware' do
+        middleware.call(nil)
+      end
     end
   end
 end
