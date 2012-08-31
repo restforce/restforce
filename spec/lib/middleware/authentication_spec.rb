727c3c637b7760c0cc8e99ade7e726c4fee606a8
Eric J. Holmes <eric@ejholmes.net>
2012-08-31 00:46:17 -0700

    Authentication spec.

---
+++ spec/lib/middleware/authentication_spec.rb
@@ -0,0 +1,67 @@
+require 'spec_helper'
+
+describe Restforce::Middleware::Authentication do
+  let(:app)        { double('app')            }
+  let(:env)        { { }  }
+  let(:options)    { { host: 'login.salesforce.com' } }
+  let(:middleware) { described_class.new app, options }
+
+  describe '.authenticate!' do
+    it 'raises an error' do
+      expect {
+        middleware.authenticate!
+      }.to raise_error RuntimeError, 'must subclass'
+    end
+  end
+
+  describe '.call' do
+    context 'when all is good' do
+      before do
+        app.should_receive(:call).once
+      end
+
+      it 'calls the next middlware' do
+        middleware.call(env)
+      end
+    end
+
+    context 'when an exception is thrown' do
+      before do
+        app.should_receive(:call).once.and_raise(Faraday::Error::ClientError.new('something bad'))
+        middleware.should_receive(:authenticate!)
+        app.should_receive(:call).once
+      end
+
+      it 'attempts to authenticate' do
+        middleware.call(env)
+      end
+    end
+  end
+
+  describe '.connection' do
+    subject { middleware.connection }
+
+    its(:url_prefix) { should eq URI.parse('https://login.salesforce.com') }
+
+    describe '.builder' do
+      subject { middleware.connection.builder }
+
+      context 'with logging disabled' do
+        before do
+          Restforce.stub!(:log?).and_return(false)
+        end
+
+        its(:handlers) { should include FaradayMiddleware::ParseJson, Faraday::Adapter::NetHttp }
+        its(:handlers) { should_not include Faraday::Response::Logger  }
+      end
+
+      context 'with logging enabled' do
+        before do
+          Restforce.stub!(:log?).and_return(true)
+        end
+
+        its(:handlers) { should include FaradayMiddleware::ParseJson, Faraday::Response::Logger, Faraday::Adapter::NetHttp }
+      end
+    end
+  end
+end
