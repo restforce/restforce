43a4733100b022a1b69e04ecb508effa3bd1c0a8
Eric J. Holmes <eric@ejholmes.net>
2012-09-03 14:36:47 -0700

    SObject.destroy and SObject.save.

--- lib/restforce/sobject.rb
+++ lib/restforce/sobject.rb
@@ -5,5 +5,37 @@ module Restforce
       self.attributes.type
     end
 
+    # Public: Persist the attributes to Salesforce.
+    #
+    # Examples
+    #
+    #   account = client.query('select Id, Name from Account').first
+    #   account.Name = 'Foobar'
+    #   account.save
+    def save
+      # Remove 'attributes' and parent/child relationships. We only want to
+      # persist the attributes on the sobject.
+      ensure_id
+      attrs = self.to_hash.reject { |key, _| key =~ /.*__r/ || key =~ /^attributes$/ }
+      @client.update(sobject_type, attrs)
+    end
+
+    # Public: Destroy this record.
+    #
+    # Examples
+    #
+    #   account = client.query('select Id, Name from Account').first
+    #   account.destroy
+    def destroy
+      ensure_id
+      @client.destroy(sobject_type, self.Id)
+    end
+
+  private
+
+    def ensure_id
+      raise 'You need to query the Id for the record in order to save.' unless self.Id?
+    end
+
   end
 end
--- spec/lib/sobject_spec.rb
+++ spec/lib/sobject_spec.rb
@@ -7,44 +7,93 @@ RSpec::Matchers.define :have_client do |expected|
 end
 
 describe Restforce::SObject do
-  let(:client) { double('client') }
+  include_context 'basic client'
+
+  let(:hash) { JSON.parse(fixture('sobject/query_success_response'))['records'].first }
+  let(:sobject) do
+    described_class.new(hash, client)
+  end
 
   describe '#new' do
     context 'with valid options' do
-      let(:record) do
-        described_class.new(JSON.parse(fixture('sobject/query_success_response'))['records'].first, client)
-      end
-
-      subject            { record }
+      subject            { sobject }
       it                 { should be_a Restforce::SObject }
       its(:sobject_type) { should eq 'Whizbang' }
       its(:Text_Label)   { should eq 'Hi there!' }
       it { should have_client client }
 
-      describe 'child records object' do
-        subject { record.Whizbangs__r }
+      describe 'children' do
+        subject { sobject.Whizbangs__r }
 
         it { should be_a Restforce::Collection }
 
-        describe 'each child record' do
+        describe 'each child' do
           it 'should be a Restforce::SObject' do
-            record.Whizbangs__r.each { |record| record.should be_a Restforce::SObject }
+            sobject.Whizbangs__r.each { |sobject| sobject.should be_a Restforce::SObject }
           end
 
           it 'should set the client' do
-            record.Whizbangs__r.each { |record| record.should have_client client }
+            sobject.Whizbangs__r.each { |sobject| sobject.should have_client client }
           end
         end
       end
 
-      describe 'parent record object' do
-        subject { record.ParentWhizbang__r }
+      describe 'parent' do
+        subject { sobject.ParentWhizbang__r }
 
         it { should be_a Restforce::SObject }
         its(:sobject_type) { should eq 'Whizbang' }
         its(:Name) { should eq 'Parent Whizbang' }
         it { should have_client client }
       end
+    end
+  end
+
+  describe '.save' do
+    subject { sobject.save }
+
+    context 'when an Id was not queried' do
+      specify { expect { subject }.to raise_error RuntimeError, 'You need to query the Id for the record in order to save.' }
+    end
+
+    context 'when an Id is present' do
+      before do
+        hash.merge!(Id: '001D000000INjVe')
+      end
+
+      before do
+        @request = stub_api_request 'sobjects/Whizbang/001D000000INjVe', method: :patch
+      end
+
+      after do
+        @request.should have_been_requested
+      end
+
+      specify { expect { subject }.to_not raise_error }
+    end
+  end
+
+  describe '.destroy' do
+    subject { sobject.destroy }
+
+    context 'when an Id was not queried' do
+      specify { expect { subject }.to raise_error RuntimeError, 'You need to query the Id for the record in order to save.' }
+    end
+
+    context 'when an Id is present' do
+      before do
+        hash.merge!(Id: '001D000000INjVe')
+      end
+
+      before do 
+        @request = stub_api_request 'sobjects/Whizbang/001D000000INjVe', method: :delete
+      end
+
+      after do
+        @request.should have_been_requested
+      end
+
+      specify { expect { subject }.to_not raise_error }
     end
   end
 end
