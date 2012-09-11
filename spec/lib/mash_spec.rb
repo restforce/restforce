830f11457baf450612327d73e21b6805eb141bdc
Eric J. Holmes <eric@ejholmes.net>
2012-09-10 21:00:41 -0700

    Mash spec.
    Fixes #12

---
+++ spec/lib/mash_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe Restforce::Mash do
+  describe '#build' do
+    subject { described_class.build(input, nil) }
+
+    context 'when array' do
+      let(:input) { [{ foo: 'hello' }, { bar: 'world' }] }
+      it 'mashifys each child object' do
+        subject.each { |obj| obj.should be_a Restforce::Mash }
+      end
+    end
+  end
+
+  describe '#klass' do
+    subject { described_class.klass(input) }
+
+    context 'when the hash has a "records" key' do
+      let(:input) { { 'records' => nil } }
+      it { should eq Restforce::Collection }
+    end
+
+    context 'when the hash has an "attributes" key' do
+      let(:input) { { 'attributes' => nil } }
+      it { should eq Restforce::SObject }
+    end
+
+    context 'else' do
+      let(:input) { {} }
+      it { should eq Restforce::Mash }
+    end
+  end
+end
