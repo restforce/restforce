3dc144007b1c750f913defedaf01ab87da31df5f
Eric J. Holmes <eric@ejholmes.net>
2012-09-11 22:21:58 -0700

    Log request.

--- lib/restforce/middleware/logger.rb
+++ lib/restforce/middleware/logger.rb
@@ -1,5 +1,12 @@
 module Restforce
   class Middleware::Logger < Faraday::Response::Logger
+    def call(env)
+      info "#{env[:method]} #{env[:url].to_s}"
+      debug('request headers') { dump_headers env[:request_headers] }
+      debug('request body') { env[:body] }
+      super
+    end
+
     def on_complete(env)
       info('Status') { env[:status].to_s }
       debug('response headers') { dump_headers env[:response_headers] }
