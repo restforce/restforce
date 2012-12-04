062cc333befde6c1ea4653454101de9fcad5ed6f
Eric J. Holmes <eric@ejholmes.net>
2012-12-04 11:50:19 -0800

    Move api_<method> methods.

--- lib/restforce/client/api.rb
+++ lib/restforce/client/api.rb
@@ -192,6 +192,61 @@ module Restforce
         true
       end
 
+      # Public: Helper methods for performing arbitrary actions against the API using
+      # various HTTP verbs.
+      #
+      # Examples
+      #
+      #   # Perform a get request
+      #   client.get '/services/data/v24.0/sobjects'
+      #   client.api_get 'sobjects'
+      #
+      #   # Perform a post request
+      #   client.post '/services/data/v24.0/sobjects/Account', { ... }
+      #   client.api_post 'sobjects/Account', { ... }
+      #
+      #   # Perform a put request
+      #   client.put '/services/data/v24.0/sobjects/Account/001D000000INjVe', { ... }
+      #   client.api_put 'sobjects/Account/001D000000INjVe', { ... }
+      #
+      #   # Perform a delete request
+      #   client.delete '/services/data/v24.0/sobjects/Account/001D000000INjVe'
+      #   client.api_delete 'sobjects/Account/001D000000INjVe'
+      #
+      # Returns the Faraday::Response.
+      [:get, :post, :put, :delete, :patch].each do |method|
+        define_method method do |*args|
+          retries = @options[:authentication_retries]
+          begin
+            connection.send(method, *args)
+          rescue Restforce::UnauthorizedError
+            if retries > 0
+              retries -= 1
+              connection.url_prefix = @options[:instance_url]
+              retry
+            end
+            raise
+          end
+        end
+
+        define_method :"api_#{method}" do |*args|
+          args[0] = api_path(args[0])
+          send(method, *args)
+        end
+      end
+
+    private
+
+      # Internal: Returns a path to an api endpoint
+      #
+      # Examples
+      #
+      #   api_path('sobjects')
+      #   # => '/services/data/v24.0/sobjects'
+      def api_path(path)
+        "/services/data/v#{@options[:api_version]}/#{path}"
+      end
+
     end
   end
 end
