cb0b2c4281e5c4fe824d0ddf1ed0849e8c6ffe6a
Eric J. Holmes <eric@ejholmes.net>
2012-12-07 15:51:04 -0800

    Cleanup

--- lib/restforce/client.rb
+++ lib/restforce/client.rb
@@ -20,35 +20,33 @@ module Restforce
     # Public: Creates a new client instance
     #
     # opts - A hash of options to be passed in (default: {}).
-    #           :username               - The String username to use (required for password authentication).
-    #           :password               - The String password to use (required for password authentication).
-    #           :security_token         - The String security token to use 
-    #                                     (required for password authentication).
+    #        :username               - The String username to use (required for password authentication).
+    #        :password               - The String password to use (required for password authentication).
+    #        :security_token         - The String security token to use (required for password authentication).
     #
-    #           :oauth_token            - The String oauth access token to authenticate api
-    #                                     calls (required unless password
-    #                                     authentication is used).
-    #           :refresh_token          - The String refresh token to obtain fresh
-    #                                     oauth access tokens (required if oauth
-    #                                     authentication is used).
-    #           :instance_url           - The String base url for all api requests
-    #                                     (required if oauth authentication is used).
+    #        :oauth_token            - The String oauth access token to authenticate api
+    #                                  calls (required unless password
+    #                                  authentication is used).
+    #        :refresh_token          - The String refresh token to obtain fresh
+    #                                  oauth access tokens (required if oauth
+    #                                  authentication is used).
+    #        :instance_url           - The String base url for all api requests
+    #                                  (required if oauth authentication is used).
     #
-    #           :client_id              - The oauth client id to use. Needed for both
-    #                                     password and oauth authentication
-    #           :client_secret          - The oauth client secret to use.
+    #        :client_id              - The oauth client id to use. Needed for both
+    #                                  password and oauth authentication
+    #        :client_secret          - The oauth client secret to use.
     #
-    #           :host                   - The String hostname to use during
-    #                                     authentication requests (default: 'login.salesforce.com').
+    #        :host                   - The String hostname to use during
+    #                                  authentication requests (default: 'login.salesforce.com').
     #
-    #           :api_version            - The String REST api version to use (default: '24.0')
+    #        :api_version            - The String REST api version to use (default: '24.0')
     #
-    #           :authentication_retries - The number of times that client
-    #                                     should attempt to reauthenticate
-    #                                     before raising an exception (default: 3).
+    #        :authentication_retries - The number of times that client
+    #                                  should attempt to reauthenticate
+    #                                  before raising an exception (default: 3).
     #
-    #           :compress               - Set to true to have Salesforce compress the
-    #                                     response (default: false).
+    #        :compress               - Set to true to have Salesforce compress the response (default: false).
     #
     # Examples
     #
@@ -58,7 +56,6 @@ module Restforce
     #     :security_token => 'security token',
     #     :client_id => 'client id',
     #     :client_secret => 'client secret'
-    #   # => #<Restforce::Client:0x007f934aa2dc28 @options={ ... }>
     #
     #   # Initialize a new client using oauth authentication:
     #   Restforce::Client.new :oauth_token => 'access token',
@@ -66,12 +63,10 @@ module Restforce
     #     :instance_url => 'https://na1.salesforce.com',
     #     :client_id => 'client id',
     #     :client_secret => 'client secret'
-    #   # => #<Restforce::Client:0x007f934aaaa0e8 @options={ ... }>
     #
-    #   # Initialize a new client with using any authentication middleware:
+    #   # Initialize a new client without using any authentication middleware:
     #   Restforce::Client.new :oauth_token => 'access token',
     #     :instance_url => 'https://na1.salesforce.com'
-    #   # => #<Restforce::Client:0x007f934aab9980 @options={ ... }>
     def initialize(opts = {})
       raise 'Please specify a hash of options' unless opts.is_a?(Hash)
       @options = Hash[OPTIONS.map { |option| [option, Restforce.configuration.send(option)] }]
