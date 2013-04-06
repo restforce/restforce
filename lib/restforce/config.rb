bf3602982f6b6ba6677337df0073a8c3e52e5e4a
Eric J. Holmes <eric@ejholmes.net>
2013-04-06 10:12:37 -0700

    refactor

--- lib/restforce/config.rb
+++ lib/restforce/config.rb
@@ -37,18 +37,48 @@ module Restforce
   end
 
   class Configuration
+    class Option
+      attr_reader :configuration, :name, :options
+
+      def self.define(*args)
+        new(*args).define
+      end
+
+      def initialize(configuration, name, options = {})
+        @configuration, @name, @options = configuration, name, options
+        @default = options.fetch(:default, nil)
+      end
+
+      def define
+        write_attribute
+        define_method if default_provided?
+        self
+      end
+
+    private
+      attr_reader :default
+      alias_method :default_provided?, :default
+
+      def write_attribute
+        configuration.send :attr_accessor, name
+      end
+
+      def define_method
+        our_default = default
+        our_name    = name
+        configuration.send :define_method, our_name do
+          instance_variable_get(:"@#{our_name}") ||
+            instance_variable_set(:"@#{our_name}", our_default.respond_to?(:call) ? our_default.call : our_default)
+        end
+      end
+    end
+
     class << self
       attr_accessor :options
 
-      def option(name, options = {})
-        default = options.fetch(:default, nil)
-        attr_accessor name
-        define_method name do
-          instance_variable_get(:"@#{name}") ||
-            instance_variable_set(:"@#{name}", default.respond_to?(:call) ? default.call : default)
-        end if default
-        self.options ||= []
-        self.options << name
+      def option(*args)
+        option = Option.define(self, *args)
+        (self.options ||= []) << option.name
       end
     end
 
