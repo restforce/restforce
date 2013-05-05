f3ce2eb9569732036e0c805eff5cbfaf490dcb55
Eric J. Holmes <eric@ejholmes.net>
2013-05-05 10:39:38 -0700

    Move initialization into Restforce::Client::Base.

--- lib/restforce/client.rb
+++ lib/restforce/client.rb
@@ -1,3 +1,4 @@
+require 'restforce/client/base'
 require 'restforce/client/connection'
 require 'restforce/client/authentication'
 require 'restforce/client/streaming'
@@ -8,6 +9,7 @@ require 'restforce/client/api'
 
 module Restforce
   class Client
+    include Restforce::Client::Base
     include Restforce::Client::Connection
     include Restforce::Client::Authentication
     include Restforce::Client::Streaming
@@ -16,72 +18,6 @@ module Restforce
     include Restforce::Client::Canvas
     include Restforce::Client::API
 
-    # Public: Creates a new client instance
-    #
-    # opts - A hash of options to be passed in (default: {}).
-    #        :username               - The String username to use (required for password authentication).
-    #        :password               - The String password to use (required for password authentication).
-    #        :security_token         - The String security token to use (required for password authentication).
-    #
-    #        :oauth_token            - The String oauth access token to authenticate api
-    #                                  calls (required unless password
-    #                                  authentication is used).
-    #        :refresh_token          - The String refresh token to obtain fresh
-    #                                  oauth access tokens (required if oauth
-    #                                  authentication is used).
-    #        :instance_url           - The String base url for all api requests
-    #                                  (required if oauth authentication is used).
-    #
-    #        :client_id              - The oauth client id to use. Needed for both
-    #                                  password and oauth authentication
-    #        :client_secret          - The oauth client secret to use.
-    #
-    #        :host                   - The String hostname to use during
-    #                                  authentication requests (default: 'login.salesforce.com').
-    #
-    #        :api_version            - The String REST api version to use (default: '24.0')
-    #
-    #        :authentication_retries - The number of times that client
-    #                                  should attempt to reauthenticate
-    #                                  before raising an exception (default: 3).
-    #
-    #        :compress               - Set to true to have Salesforce compress the response (default: false).
-    #        :timeout                - Faraday connection request read/open timeout. (default: nil).
-    #
-    #        :proxy_uri              - Proxy URI: 'http://proxy.example.com:port' or 'http://user@pass:proxy.example.com:port'
-    #
-    # Examples
-    #
-    #   # Initialize a new client using password authentication:
-    #   Restforce::Client.new :username => 'user',
-    #     :password => 'pass',
-    #     :security_token => 'security token',
-    #     :client_id => 'client id',
-    #     :client_secret => 'client secret'
-    #
-    #   # Initialize a new client using oauth authentication:
-    #   Restforce::Client.new :oauth_token => 'access token',
-    #     :refresh_token => 'refresh token',
-    #     :instance_url => 'https://na1.salesforce.com',
-    #     :client_id => 'client id',
-    #     :client_secret => 'client secret'
-    #
-    #   # Initialize a new client without using any authentication middleware:
-    #   Restforce::Client.new :oauth_token => 'access token',
-    #     :instance_url => 'https://na1.salesforce.com'
-    #
-    def initialize(opts = {})
-      raise 'Please specify a hash of options' unless opts.is_a?(Hash)
-      @options = Hash[Restforce.configuration.options.map { |option| [option, Restforce.configuration.send(option)] }]
-      @options.merge! opts
-      yield builder if block_given?
-    end
-
-    def instance_url
-      authenticate! unless @options[:instance_url]
-      @options[:instance_url]
-    end
-
     # Public: Returns a url to the resource.
     #
     # resource - A record that responds to to_sparam or a String/Fixnum.
@@ -89,10 +25,6 @@ module Restforce
     # Returns the url to the resource.
     def url(resource)
       "#{instance_url}/#{(resource.respond_to?(:to_sparam) ? resource.to_sparam : resource)}"
-    end
-
-    def inspect
-      "#<#{self.class} @options=#{@options.inspect}>"
     end
   end
 end
