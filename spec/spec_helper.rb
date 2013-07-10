ea127576b79b882255f2b17f77827a24036cf449
Chulki Lee <chulki@asperasoft.com>
2013-07-10 00:19:22 -0700

    Use Ruby 1.8 hash syntax

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,7 +11,7 @@ WebMock.disable_net_connect!
 
 RSpec.configure do |config|
   config.order = 'random'
-  config.filter_run focus: true
+  config.filter_run :focus => true
   config.run_all_when_everything_filtered = true
 end
 
