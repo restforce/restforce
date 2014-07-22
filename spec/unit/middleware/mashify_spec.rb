e426bd2cc0a1ea223a5b23d97c026587a6a58fa7
Dan Wanek <dan@crisply.com>
2014-07-22 03:57:40 +0000

    Use callback in Mashify middleware.

--- spec/unit/middleware/mashify_spec.rb
+++ spec/unit/middleware/mashify_spec.rb
@@ -2,6 +2,11 @@ require 'spec_helper'
 
 describe Restforce::Middleware::Mashify do
   let(:env) { { :body => JSON.parse(fixture('sobject/query_success_response')) } }
+  subject(:middleware) {
+    described_class.new(lambda {|env|
+      Faraday::Response.new(env)
+    }, client, options)
+  }
 
   describe '.call' do
     subject { lambda { middleware.call(env) } }
