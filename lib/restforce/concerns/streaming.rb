1ba2e945493dbd0c038670d63a00c2f10b297de2
Anthony Panozzo <panozzaj@gmail.com>
2016-10-20 22:53:22 -0400

    Implement very simple push topic replay
    This adds an options hash to the `subscribe` method on clients, which
    allows us to pass a message id to replay from. This lets us use
    Salesforce's ability to replay PushTopic events.

--- lib/restforce/concerns/streaming.rb
+++ lib/restforce/concerns/streaming.rb
@@ -7,8 +7,10 @@ module Restforce
       # block    - A block to run when a new message is received.
       #
       # Returns a Faye::Subscription
-      def subscribe(channels, &block)
-        faye.subscribe Array(channels).map { |channel| "/topic/#{channel}" }, &block
+      def subscribe(channels, options = {}, &block)
+        @replay = options[:replay]
+        @channels = Array(channels)
+        faye.subscribe @channels.map { |channel| "/topic/#{channel}" }, &block
       end
 
       # Public: Faye client to use for subscribing to PushTopics
@@ -19,17 +21,49 @@ module Restforce
 
         url = "#{options[:instance_url]}/cometd/#{options[:api_version]}"
 
-        @faye ||= Faye::Client.new(url).tap do |client|
-          client.set_header 'Authorization', "OAuth #{options[:oauth_token]}"
+        unless @faye
+          @faye = Faye::Client.new(url).tap do |client|
+            client.set_header 'Authorization', "OAuth #{options[:oauth_token]}"
 
-          client.bind 'transport:down' do
-            Restforce.log "[COMETD DOWN]"
-            client.set_header 'Authorization', "OAuth #{authenticate!.access_token}"
+            client.bind 'transport:down' do
+              Restforce.log "[COMETD DOWN]"
+              client.set_header 'Authorization', "OAuth #{authenticate!.access_token}"
+            end
+
+            client.bind 'transport:up' do
+              Restforce.log "[COMETD UP]"
+            end
           end
 
-          client.bind 'transport:up' do
-            Restforce.log "[COMETD UP]"
+          if @replay
+            @faye.add_extension ReplayExtension.new(channels: @channels, replay: @replay)
           end
+        end
+
+        @faye
+      end
+
+      class ReplayExtension
+        def initialize options
+          @channels = options[:channels]
+          @replay = options[:replay]
+        end
+
+        def outgoing message, callback
+          # Leave non-subscribe messages alone
+          unless message['channel'] == '/meta/subscribe'
+            return callback.call(message)
+          end
+
+          # Set the replay value for the each channel
+          message['ext'] ||= {}
+          message['ext']['replay'] = {}
+          @channels.each do |channel|
+            message['ext']['replay']["/topic/#{channel}"] = @replay
+          end
+
+          # Carry on and send the message to the server
+          callback.call message
         end
       end
     end
