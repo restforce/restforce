85736a23798a6222a250af9050ef206e8ef8e315
Igor Zubkov <igor.zubkov@gmail.com>
2018-05-30 23:36:49 +0300

    Fix indent

--- lib/restforce/concerns/connection.rb
+++ lib/restforce/concerns/connection.rb
@@ -72,7 +72,7 @@ module Restforce
         { request: {
             timeout: options[:timeout],
             open_timeout: options[:timeout]
-},
+          },
           proxy: options[:proxy_uri],
           ssl: options[:ssl] }
       end
