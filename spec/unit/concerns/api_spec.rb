restforce/restforce

commit b3a52e8cfcfd20af727b3c8449ffc599774e3035

Manually fix Rubocop offences and RSpec failures

lib/restforce/concerns/api.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'uri'
 require 'restforce/concerns/verbs'
 
@@ -378,7 +380,7 @@ module Restforce
             api_patch "sobjects/#{sobject}/#{field}/#{CGI.escape(external_id)}", attrs
           end
 
-        response.body && response.body['id'] ? response.body['id'] : true
+        response&.body&.fetch('id', nil) ? response.body['id'] : true
       end
 
       # Public: Delete a record.
@@ -448,7 +450,8 @@ module Restforce
                else
                  "sobjects/#{sobject}/#{CGI.escape(id)}"
                end
-        path << "?fields=#{select.join(',')}" if select && select.any?
+
+        path = "#{path}?fields=#{select.join(',')}" if select&.any?
 
         api_get(path).body
       end

spec/unit/concerns/api_spec.rb
@@ -344,7 +344,7 @@ describe Restforce::Concerns::API do
 
       context 'when the record is found and updated' do
         it 'returns true' do
-          response.body.stub :[]
+          response.stub(:body) { {} }
           client.should_receive(:api_patch).
             with('sobjects/Whizbang/External_ID__c/1234', {}).
             and_return(response)
@@ -354,7 +354,7 @@ describe Restforce::Concerns::API do
 
       context 'when the record is found and created' do
         it 'returns the id of the record' do
-          response.body.stub(:[]).with('id').and_return('4321')
+          response.stub(:body) { { "id" => "4321" } }
           client.should_receive(:api_patch).
             with('sobjects/Whizbang/External_ID__c/1234', {}).
             and_return(response)
@@ -378,7 +378,7 @@ describe Restforce::Concerns::API do
 
         context 'and the value for Id is provided' do
           it 'returns the id of the record, and original record still contains id' do
-            response.body.stub(:[]).with('id').and_return('4321')
+            response.stub(:body) { { "id" => "4321" } }
             client.should_receive(:api_patch).
               with('sobjects/Whizbang/Id/4321', {}).
               and_return(response)
@@ -391,7 +391,7 @@ describe Restforce::Concerns::API do
           let(:attrs) { { 'External_ID__c' => '1234' } }
 
           it 'uses POST to create the record' do
-            response.body.stub(:[]).with('id').and_return('4321')
+            response.stub(:body) { { "id" => "4321" } }
             client.should_receive(:options).and_return(api_version: 38.0)
             client.should_receive(:api_post).
               with('sobjects/Whizbang/Id', attrs).
@@ -417,7 +417,7 @@ describe Restforce::Concerns::API do
 
       context 'when the record is found and updated' do
         it 'returns true' do
-          response.body.stub :[]
+          response.stub(:body) { {} }
           client.should_receive(:api_patch).
             with('sobjects/Whizbang/External_ID__c/%E3%81%82', {}).
             and_return(response)
@@ -434,7 +434,7 @@ describe Restforce::Concerns::API do
 
       context 'when the record is found and updated' do
         it 'returns true' do
-          response.body.stub :[]
+          response.stub(:body) { {} }
           client.should_receive(:api_patch).
             with('sobjects/Whizbang/External_ID__c/1234', {}).
             and_return(response)

lib/restforce/concerns/caching.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Restforce
   module Concerns
     module Caching

lib/restforce/concerns/connection.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Restforce
   module Concerns
     module Connection
@@ -23,40 +25,40 @@ module Restforce
                                     connection_options) do |builder|
           # Parses JSON into Hashie::Mash structures.
           unless options[:mashify] == false
-            builder.use    Restforce::Middleware::Mashify, self, options
+            builder.use Restforce::Middleware::Mashify, self, options
           end
 
           # Handles multipart file uploads for blobs.
-          builder.use      Restforce::Middleware::Multipart
+          builder.use Restforce::Middleware::Multipart
           # Converts the request into JSON.
-          builder.request  :json
+          builder.request :json
           # Handles reauthentication for 403 responses.
           if authentication_middleware
-            builder.use    authentication_middleware, self, options
+            builder.use authentication_middleware, self, options
           end
           # Sets the oauth token in the headers.
-          builder.use      Restforce::Middleware::Authorization, self, options
+          builder.use Restforce::Middleware::Authorization, self, options
           # Ensures the instance url is set.
-          builder.use      Restforce::Middleware::InstanceURL, self, options
+          builder.use Restforce::Middleware::InstanceURL, self, options
           # Caches GET requests.
-          builder.use      Restforce::Middleware::Caching, cache, options if cache
+          builder.use Restforce::Middleware::Caching, cache, options if cache
           # Follows 30x redirects.
-          builder.use      FaradayMiddleware::FollowRedirects
+          builder.use FaradayMiddleware::FollowRedirects
           # Raises errors for 40x responses.
-          builder.use      Restforce::Middleware::RaiseError
+          builder.use Restforce::Middleware::RaiseError
           # Parses returned JSON response into a hash.
           builder.response :json, content_type: /\bjson$/
           # Compress/Decompress the request/response
           unless adapter == :httpclient
-            builder.use      Restforce::Middleware::Gzip, self, options
+            builder.use Restforce::Middleware::Gzip, self, options
           end
           # Inject custom headers into requests
-          builder.use      Restforce::Middleware::CustomHeaders, self, options
+          builder.use Restforce::Middleware::CustomHeaders, self, options
           # Log request/responses
           if Restforce.log?
-            builder.use      Restforce::Middleware::Logger,
-                             Restforce.configuration.logger,
-                             options
+            builder.use Restforce::Middleware::Logger,
+                        Restforce.configuration.logger,
+                        options
           end
 
           builder.adapter adapter

lib/restforce/concerns/picklists.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Restforce
   module Concerns
     module Picklists
@@ -82,7 +84,7 @@ module Restforce
         def valid?(picklist_entry)
           valid_for = picklist_entry['validFor'].ljust(16, 'A').unpack('m').first.
                       unpack('C*')
-          (valid_for[index >> 3] & (0x80 >> index % 8)) > 0
+          (valid_for[index >> 3] & (0x80 >> index % 8)).positive?
         end
       end
     end

lib/restforce/concerns/verbs.rb
@@ -36,7 +36,7 @@ module Restforce
           begin
             connection.send(verb, *args, &block)
           rescue Restforce::UnauthorizedError
-            if retries > 0
+            if retries.positive?
               retries -= 1
               connection.url_prefix = options[:instance_url]
               retry

spec/integration/abstract_client_spec.rb
@@ -170,7 +170,8 @@ shared_examples_for Restforce::AbstractClient do
     context 'when updated' do
       requests 'sobjects/Account/External__c/foobar',
                method: :patch,
-               with_body: "{\"Name\":\"Foobar\"}"
+               with_body: "{\"Name\":\"Foobar\"}",
+               fixture: "sobject/upsert_updated_success_response"
 
       context 'with symbol external Id key' do
         subject do

commit 92b762da2a9dc2d44fbbb0dffd41076e55b1f19c

upsert! to handle String response body gracefully
This fixes a regression introduced in b3a52e8cfcfd for the case when
response body is not a Hash.

lib/restforce/concerns/api.rb
@@ -380,7 +380,7 @@ module Restforce
             api_patch "sobjects/#{sobject}/#{field}/#{CGI.escape(external_id)}", attrs
           end
 
-        response&.body&.fetch('id', nil) ? response.body['id'] : true
+        response.body.respond_to?(:fetch) ? response.body.fetch('id', true) : true
       end
 
       # Public: Delete a record.

spec/unit/concerns/api_spec.rb
@@ -350,6 +350,16 @@ describe Restforce::Concerns::API do
             and_return(response)
           expect(result).to be_true
         end
+
+        context 'and the response body is a string' do
+          it 'returns true' do
+            response.stub(:body) { '' }
+            client.should_receive(:api_patch).
+              with('sobjects/Whizbang/External_ID__c/1234', {}).
+              and_return(response)
+            expect(result).to be_true
+          end
+        end
       end
 
       context 'when the record is found and created' do