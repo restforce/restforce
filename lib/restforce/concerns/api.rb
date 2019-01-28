0211ed96050b9b9ae7e21d398e6bb593effce6e7
Yuri Kasperovich <ykas.gg@gmail.com>
2019-01-28 20:04:32 +0300

    Minor documentation fixes

--- lib/restforce/concerns/api.rb
+++ lib/restforce/concerns/api.rb
@@ -355,7 +355,7 @@ module Restforce
       # Examples
       #
       #   # Add new accounts
-      #   client.create_collection([{Name: 'Foo Inc.'}, {Name: 'Bar Corp.'}])
+      #   client.create_collection!([{Name: 'Foo Inc.'}, {Name: 'Bar Corp.'}])
       #   # => [{ 'id' => '1', 'success' => true, 'errors' => [] },
       #   #     { 'id' => '2', 'success' => true, 'errors' => [] }]
       #
@@ -396,7 +396,7 @@ module Restforce
       # Examples
       #
       #   # Update accounts
-      #   client.update_collection([{Id: '1', Name: 'Foo Inc.'}, {Id: '2', Name: 'Bar Corp.'}])
+      #   client.update_collection!([{Id: '1', Name: 'Foo Inc.'}, {Id: '2', Name: 'Bar Corp.'}])
       #   # => [{ 'id' => '1', 'success' => true, 'errors' => [] },
       #   #     { 'id' => '2', 'success' => true, 'errors' => [] }]
       #
@@ -417,7 +417,7 @@ module Restforce
       # Examples
       #
       #   # Delete collection of records
-      #   client.destroy('0016000000MRatd', '0016000000ERats')
+      #   client.destroy_collection('0016000000MRatd', '0016000000ERats')
       #
       # Returns array of result objects.
       # Returns false if an error is returned from Salesforce.
@@ -435,7 +435,7 @@ module Restforce
       # Examples
       #
       #   # Delete collection of records
-      #   client.destroy('0016000000MRatd', '0016000000ERats')
+      #   client.destroy_collection!('0016000000MRatd', '0016000000ERats')
       #
       # Returns array of result objects.
       # Raises exceptions if an error is returned from Salesforce.
