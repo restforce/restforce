8f13a8cb7a4797ad207c6942f90eda04a567245d
Tim Rogers <tim@duffel.com>
2019-10-09 22:00:40 +0100

    Autocorrect Rubocop offences

--- lib/restforce/concerns/picklists.rb
+++ lib/restforce/concerns/picklists.rb
@@ -83,7 +83,7 @@ module Restforce
         # See http://www.salesforce.com/us/developer/docs/api/Content/sforce_api_calls_des
         # cribesobjects_describesobjectresult.htm
         def valid?(picklist_entry)
-          valid_for = picklist_entry['validFor'].ljust(16, 'A').unpack('m').first.
+          valid_for = picklist_entry['validFor'].ljust(16, 'A').unpack1('m').
                       unpack('C*')
           (valid_for[index >> 3] & (0x80 >> index % 8)).positive?
         end
