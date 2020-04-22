e9a5a851c7abdeb8eb5350efbdbc75a95d4b98d5
Flavio Visetti Soares <flavio.visetti@gmail.com>
2020-04-22 13:31:36 -0300

    Change the Faraday::UploadIO for the new method

--- lib/restforce/upload_io.rb
+++ lib/restforce/upload_io.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Restforce
-  UploadIO = Faraday::UploadIO
+  UploadIO = Faraday::FilePart
 end
 
 require 'restforce/patches/parts' unless Parts::Part.method(:new).arity.abs == 4
