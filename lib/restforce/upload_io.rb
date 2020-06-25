e4d31904e952247854aff2178e59ea457b999dd5
Ryan Schlesinger <ryan@ryanschlesinger.com>
2020-06-25 12:11:46 -0700

    Add Restforce::FilePart to track faraday changes

--- lib/restforce/upload_io.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'faraday/upload_io'
-
-module Restforce
-  UploadIO = Faraday::UploadIO
-end
-
-require 'restforce/patches/parts' unless Parts::Part.method(:new).arity.abs == 4
