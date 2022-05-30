37ebb78525c1a19aa75f107b041956f4dd6e6b45
Tim Rogers <me@timrogers.co.uk>
2022-05-30 18:37:41 +0100

    Switch away from `faraday_middleware` for the "follow redirects" middleware

--- lib/restforce/concerns/connection.rb
+++ lib/restforce/concerns/connection.rb
@@ -43,7 +43,7 @@ module Restforce
           # Caches GET requests.
           builder.use Restforce::Middleware::Caching, cache, options if cache
           # Follows 30x redirects.
-          builder.use FaradayMiddleware::FollowRedirects
+          builder.use Faraday::FollowRedirects::Middleware
           # Raises errors for 40x responses.
           builder.use Restforce::Middleware::RaiseError
           # Parses returned JSON response into a hash.
