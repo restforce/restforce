6aa74f23bbc8d7a7c231ee611967e10157e02ac7
Kenneth Yang <hykenneth@gmail.com>
2022-09-21 01:15:45 -0400

    add tests

--- lib/restforce/concerns/bulk_api.rb
+++ lib/restforce/concerns/bulk_api.rb
@@ -1,30 +1,46 @@
 # frozen_string_literal: true
 
 require 'restforce/concerns/verbs'
+require 'csv'
 
 module Restforce
   module Concerns
     module BulkAPI
+      module JobState
+        OPEN = 'Open'
+        UPLOAD_COMPLETED = 'UploadComplete'
+        ABORTED = 'Aborted'
+        JOB_COMPLETED = 'JobComplete'
+        FAILED = 'Failed'
+      end
+
       extend Restforce::Concerns::Verbs
 
-      define_verbs :post
+      define_verbs :post, :put, :patch
 
-      def bulk_upsert(sobject_name, external_key, _data)
-        create_job_response = api_post(
-          UpsertJob.api_path,
+      def bulk_upsert(sobject_name, external_key, data)
+        response_on_create = api_post(
+          'jobs/ingest',
           UpsertJob.create_params(sobject_name, external_key).to_json
         )
 
-        job = UpsertJob.new(create_job_response.body)
+        job = UpsertJob.new(response_on_create.body)
+
+        api_put(
+          "#{job.api_path}/batches",
+          job.prepare_upload_content(data)
+        )
+
+        api_patch(
+          job.api_path,
+          job.patch_state_params(JobState::UPLOAD_COMPLETED).to_json
+        )
+
         job
       end
 
       class UpsertJob
         attr_reader :id, :object_name, :external_key
-
-        def self.api_path
-          'jobs/ingest'
-        end
 
         def self.create_params(sobject_name, external_key)
           {
@@ -36,8 +52,23 @@ module Restforce
           }
         end
 
-        def initialize(create_job_response_body)
-          @id = create_job_response_body['id']
+        def initialize(response_body)
+          @id = response_body['id']
+          @object_name = response_body['object']
+          @external_key = response_body['externalIdField']
+        end
+
+        def api_path
+          "jobs/#{id}"
+        end
+
+        def patch_state_params(state)
+          { 'state' => state }
+        end
+
+        def prepare_upload_content(_data)
+          # TODO: turn it into a CSV
+          []
         end
       end
     end
--- spec/unit/concerns/bulk_api_spec.rb
+++ spec/unit/concerns/bulk_api_spec.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
+require 'csv'
 
 describe Restforce::Concerns::BulkAPI do
   let(:endpoint) { 'jobs/ingest' }
 
   describe '.bulk_upsert' do
-    let(:response) do
+    let(:response_on_create) do
       double('Faraday::Response', body: {
                'id' => 'abc',
                'operation' => 'upsert',
@@ -14,6 +15,9 @@ describe Restforce::Concerns::BulkAPI do
                'externalIdFieldName' => 'bar'
              })
     end
+
+    let(:response_on_put) { double('Faraday::Response', body: {}) }
+    let(:response_on_patch) { double('Faraday::Response', body: {}) }
 
     before do
       create_job_payload = {
@@ -27,7 +31,17 @@ describe Restforce::Concerns::BulkAPI do
       allow(client).
         to receive(:api_post).
         with(endpoint, create_job_payload.to_json).
-        and_return(response)
+        and_return(response_on_create)
+
+      allow(client).
+        to receive(:api_put).
+        with('jobs/abc/batches', []).
+        and_return(response_on_put)
+
+      allow(client).
+        to receive(:api_patch).
+        with('jobs/abc', { 'state' => 'UploadComplete' }.to_json).
+        and_return(response_on_put)
     end
 
     it 'returns the UpsertJob' do
