fe69addfa3eca66b47ddb5fb9aea56016346cc9b
Diego Scataglini <dscataglini@hedgeye.com>
2024-09-19 20:16:16 -0400

    renaming option from :debug to :dry_run
    Co-authored-by: Tim Rogers <timrogers@github.com>

--- lib/restforce/concerns/sobject_collection_api.rb
+++ lib/restforce/concerns/sobject_collection_api.rb
@@ -37,7 +37,7 @@ module Restforce
         all_or_none = opts.fetch(:all_or_none, false)
         builder = RecordsBuilder.new
         yield(builder)
-        return builder.records if opts[:debug]
+        return builder.records if opts[:dry_run]
 
         if builder.records.empty?
           raise ArgumentError, "There are no records to be created"
